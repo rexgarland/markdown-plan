64c4bbe77e0a7757c5cba2c70202ddff2a508694
Rex Garland <rex@rexgarland.dev>
2023-07-22 00:33:32 -0700

    fix: auto

--- javascript/__tests__/count.test.js
+++ javascript/__tests__/count.test.js
@@ -4,7 +4,12 @@ describe("counting tasks", () => {
   it("should count a header", () => {
     const plan = "# hello";
     const { total } = countTasks(plan);
+    expect(total).toBe(1);
+  });
 
+  it("should count a list item", () => {
+    const plan = "- hello";
+    const { total } = countTasks(plan);
     expect(total).toBe(1);
   });
 });
--- javascript/count.js
+++ javascript/count.js
@@ -1,3 +1,13 @@
+function isTask(line) {
+  return line.startsWith("# ") || line.startsWith("- ");
+}
+
 export function countTasks(plan) {
-  return 0;
+  const lines = plan.split("\n");
+  const taskLines = lines.filter(isTask);
+
+  const total = taskLines.length;
+  const completed = 0;
+
+  return { total, completed };
 }
